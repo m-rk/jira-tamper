badb5e5abafa6d499cd1289620f0b96e402abb95
Mark <mark+github@tan.gl>
2016-10-24 14:11:32 +0800

    Create jira-lo-res-reports.user.js

---
+++ jira-lo-res-reports.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         JIRA Lo-Res Agile Reports
+// @namespace    http://tan.gl/w/mark
+// @version      0.1
+// @description  Increase size of report elements to suit low resolution screenshots
+// @author       Mark D
+// @match        https://navitasltd.atlassian.net/secure/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+GM_addStyle('path.work-line, path.guide-line, path.scope-line, path.scope-projection-line { stroke-width: 20px !important; }');
